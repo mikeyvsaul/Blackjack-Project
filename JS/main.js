1fb70aebae6e5f208ab942a51ea9473b6e6a429b
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-08-06 14:19:49 -0700

    Card CSS Baseline

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,21 @@
 /*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+const masterDeck = buildMasterDeck();
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+function buildMasterDeck() {
+  const deck = [];
+  suits.forEach(function (suit) {
+    ranks.forEach(function (rank) {
+      deck.push({
+        face: `${suit}${rank}`,
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
+    });
+  });
+  return deck;
+}
